33169b573b05d5a47b21c6b5c25920eb205b69e1
bujowskis <s.bujowski@gmail.com>
2024-10-13 14:59:17 +0200

    3d visualization fixup

--- visualization3d.py
+++ visualization3d.py
@@ -5,4 +5,4 @@ from assignment1.nearest_neighbor_at_any import nearest_neighbor_at_any_solve
 if __name__ == "__main__":
     tsp = TSP.load_tspa()
     solution = nearest_neighbor_at_any_solve(tsp)
-    tsp.visualize_solution3d(solution, 'foo', 'experiments_results-3d-nn-any.png')
+    tsp.visualize_solution3d(solution, 'NN any (some solution)', 'experiments_results-3d-nn-any.png')
